8e32d790c3ce1efb902aca756d58beac5e090121
Michael <mswans9@wgu.edu>
2016-06-16 15:43:22 -0500

    Small changes in formatting, primarily in comments

--- mineyelp.py
+++ mineyelp.py
@@ -1,16 +1,18 @@
 #!/bin/python
 
-########
-# This application is being made in order for me to learn 'requests' and 'BeautifulSoup'
+############################################################
+# This application is being made in order for me
+#     to learn 'requests' and 'BeautifulSoup'
 # It is intended to be functional for limited purposes.
-# Primary use case: quickly find a restaurant in Yelp and display their most popular items.
-#######
+# Objective: quickly find a restaurant in Yelp and
+#     display their most popular items.
+############################################################
 
 import requests
 from bs4 import BeautifulSoup
 
-desc = raw_input('Food/Restaurant: ')
-loc = raw_input('Geo: ')
+desc = input('Food/Restaurant: ')
+loc = input('Geo: ')
 
 payload = {'find_desc': desc, 'find_loc': loc}
 
@@ -18,8 +20,8 @@ r = requests.get('http://www.yelp.com/search?', params = payload)
 
 data = r.text
 
-print r.url
-print r.status_code
+print(r.url)
+print(r.status_code)
 soup = BeautifulSoup(data,'html.parser')
 
 bizlinks = []
@@ -28,15 +30,18 @@ for link in soup.findAll('a',{'class': 'biz-name'}):
         bizlinks.append('http://wwww.yelp.com' + link.get('href'))
 
 bizlinks = bizlinks[1:]
+bizlinks = bizlinks[:1]
 
 for i in bizlinks:
-        print i
+        print(i)
         r = requests.get(i)
         data = r.text
         soup = BeautifulSoup(data,'html.parser')
-# Grab restaurant name
+        
+        # Grab restaurant name
         for title in soup.findAll('h1',{'class': 'biz-page-title'}):
-                print('Restaurant: ' + title.get_text())
-# Grab top mentioned phrases
-        for ngram in soup.findAll('a',{'class': 'ngram'}):
-                print('Most talked about features: ' + ngram.get_text() + '\n')
+            print('Restaurant: ' + title.get_text())
+            
+        # Grab top mentioned phrases
+        for ngram in soup.findAll('p',{'class': 'quote'}):
+            print('Most talked about features: ' + ngram.get_text() + '\n')
